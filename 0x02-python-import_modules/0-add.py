47d220a840236bdb28d26aefc0f62206c7e58425
Williams Michael <williamsmicheal237@gmail.com>
2024-01-04 17:37:02 -0800

    0x02

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
-
-if __name__ == "__main__":
-    """Print the sum of 1 and 2."""
-    from add_0 import add
-
-    a = 1
-    b = 2
-    print("{} + {} = {}".format(a, b, add(a, b)))
+a = 1
+b = 2
+add_0 = __import__("add_0").add
+print("{} + {} = {}".format(a, b, add_0(a, b)))
