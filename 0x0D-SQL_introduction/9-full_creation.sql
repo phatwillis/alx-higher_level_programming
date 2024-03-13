a3d8450604c87482beded42c0774670ff2de92bb
Williams Michael <williamsmicheal237@gmail.com>
2024-03-12 19:09:15 -0700

    sql

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,6 @@
+-- Creates and fills a table second_table with attributes id, name and score in my MySQL server with multiple rows.
+CREATE TABLE IF NOT EXISTS `second_table` (`id` INT, `name` VARCHAR(256), `score` INT);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, "Samuel", 10);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (2, "Michael", 13);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (3, "Williams", 11);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (4, "Mark", 7);
